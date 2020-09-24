4080a2d38cb9b326390547e155d531b2d99f3ece
Sarah <smfrost@ucsc.edu>
2020-09-24 14:58:29 -0400

    Rewrote gradient script

--- static/js/gradient_script_revised9_24.js
+++ static/js/gradient_script_revised9_24.js
@@ -1,4 +1,30 @@
 
+/*
+Process order:
+0. get filepaths for images from CSV
+1a. Function to get two random anchor images
+1b. print affect vectors for anchor images
+1c. Set up refresh buttons for anchor images
+
+2a. get all gradient blends between two anchor images and save to array
+2b. print the weight and gradient blends
+
+3a. run lookup function for all gradient blends, ensure there are no duplicates,
+save file names to another array
+3b. print file names and affect vectors
+
+
+4a. Build Function to move gradient images across screen
+4b. set up "more like this" buttons to move between anchor images
+
+
+*/
+
+
+/*
+0. get filepaths for images from CSV
+*/
+
 var csvFile = "../data/images.csv";
 
 var liwc_csv = "../data/liwc_toParse.csv";
@@ -52,60 +78,46 @@ function LIWC_CSVToArray(path)
 }
 var liwc_filePaths = LIWC_CSVToArray(liwc_csv);
 
-// SF 9/24 - Changing code so that the beginning target image is not random.
-
+/*
+1a. Function to get two random anchor images
+*/
 
 $(document).ready(function() {
     //Takes a divID, and puts a random image in that div
     function getBeginningImage(divID){
-
-
         filePaths = readCSVToArray(csvFile);
-
         var randomNumber = Math.floor(Math.random() * filePaths.length);
         randomImage = filePaths[randomNumber];
 
+        var butName = divID + 'but';
+        $(butName).empty();
+        var x = document.createElement('img');
+        x.src = '../imgs/images/' + randomImage;
 
-        if (divID == "#target"){
-            $(divID).empty();
-            myInnerDiv = document.createElement('div');
-            myInnerDiv.setAttribute('id', 'myInnerDiv');
+        var divName = divID.replace("#","");
+        x.setAttribute('id', divName+ 'img');
 
-            myImage = document.createElement('img');
-            myImage.src = '../imgs/images/' + randomImage;
-            var divName = divID.replace("#","");
-            myImage.setAttribute('id', divName+ 'img');
+        $(butName).prepend(x);
 
-            $(myInnerDiv).append(myImage);
-            $(divID).append(myInnerDiv);
-        }
-        else {
-            var butName = divID + 'but';
-            $(butName).empty();
-            var x = document.createElement('img');
-            x.src = '../imgs/images/' + randomImage;
+        for (i=0; i < liwc_filePaths.length; i++){
+            if (liwc_filePaths[i][0] == randomImage){
+                var affVec = liwc_filePaths[i].slice(1, 6);
+                console.log(randomImage + affVec);
+                $('#AffectBlend1').append(randomImage + " " + affVec + "<br>")
 
-            var divName = divID.replace("#","");
-            x.setAttribute('id', divName+ 'img');
-
-            $(butName).prepend(x);
-
-            for (i=0; i < liwc_filePaths.length; i++){
-                if (liwc_filePaths[i][0] == randomImage){
-                    var affVec = liwc_filePaths[i].slice(1, 6);
-                    console.log(randomImage + affVec);
-                    $('#AffectBlend1').append(randomImage + " " + affVec + "<br>")
-
-                };
             };
-        }
+        };
         return affVec;
     };
 
-    //getBeginningImage('#target');
+
+
     var anchor1img_affVec = getBeginningImage('#anchor1');
     var anchor2img_affVec = getBeginningImage('#anchor2');
 
+/*
+1b. print affect vectors for anchor images
+*/
 
     //NEW CODE TO DISPLAY AFFECT VECTORS
     newDiv1 = document.createElement("div1");
@@ -113,6 +125,9 @@ $(document).ready(function() {
 
 
 
+/*
+1c. Set up refresh buttons for anchor images
+*/
 
     $('#mbutton1').click(function(){
         getBeginningImage('#anchor1');
@@ -128,79 +143,94 @@ $(document).ready(function() {
     });
 
 
+
 /*
-For C0, C1 either the affect vector or the position vector of the anchor images
-
-where a is [.1,.2,.3,.4,.5,.6,.7,.8,.9]
-C = (1-a)C0 + (a)C1
-
-C1 = right
-C0 = left
+2a. get all gradient blends between two anchor images and save to array
 */
 
-    function getVectorStep(a){
-        //var a = [.1,.2,.3,.4,.5,.6,.7,.8,.9]
-        var c0 = anchor1img_affVec;
-        var c1 = anchor2img_affVec;
+function getGradientBlends{
 
-        var c0_num = [];
-        var c1_num = [];
+    weight = [.9,.8,.7,.6,.5,.4,.3,.2,.1]
+    opp_weight = [.1,.2,.3,.4,.5,.6,.7,.8,.9]
 
-        var c0plusc1 = [];
+    //array of arrays of gradient blends
+    gradient_blends = []
+    // array of affects for one gradient blend
+    gradient_blend = []
 
-    //c0, image on left
-        for(var i=0; i<5; i++) {
-            imgInt = parseInt(c0[i]);
-            imgInt *= (1-a);
-            c0_num.push( imgInt );
+    for(var i=0; i<weight.length; i++){
+        for (var k=0; k<5; k++){
+            grad_piece = weight[i] * anchor1img_affVec[k] + opp_weight[i] * anchor2img_affVec[k]
+            gradient_blend.push[grad_piece]
         }
-        console.log('c0num', c0_num);
+        gradient_blends.push[gradient_blend]
+    }
+    return gradient_blends;
+    console.log(gradient_blends)
+    /*
+    2b. print the weight and gradient blends
+    */
+
+}
+
+gradient_blends = getGradientBlends()
+
+/*
+3a. run lookup function for all gradient blends, ensure there are no duplicates,
+save file names to another array
+*/
 
 
-    //c1, image on right
-        for(var i=0; i<5; i++) {
-            imgInt = parseInt(c1[i]);
-            imgInt *= a;
-            c1_num.push( imgInt );
+//array of gradient image names
+image_names = []
+
+    function getGradientImages(gradient_blends){
+        for(var i=0; i<gradient_blends.length; i++){
+            var pred = {"positive": gradient_blends[i][0], "anxiety": gradient_blends[i][1], "anger": gradient_blends[i][2],
+            "sad": gradient_blends[i][3], "affiliation": gradient_blends[i][4]}
+            // run getGradientImage with each pred
+            img_src = getGradientImage(pred);
+            // TO DO - have to ensure there are no duplicates!
+            image_names.push(img_src);
         }
-        console.log('c1num', c1_num);
 
-        for(var i=0; i<5; i++) {
-            imgInt = (c0_num[i] + c1_num[i]);
-            c0plusc1.push(imgInt);
-
-        }
-
-
-    var pred = {"positive": c0plusc1[0],
-    "anxiety": c0plusc1[1],
-    "anger": c0plusc1[2],
-    "sad": c0plusc1[3],
-    "affiliation": c0plusc1[4]}
-
-    console.log("pred is", pred);
-
-
-
-    $('#AffectBlend2').append("affect blend is " + c0plusc1 + "<br> ")
-
-
-    return pred
-
-
+    return(image_names)
     }
 
-    function newTargetImage(a){
-        pred = getVectorStep(a);
+    function getGradientImage(pred){
         $.ajax({
-        url: "/GradientArt",
-        type: 'POST',
-        data: JSON.stringify (pred),
-        contentType: "application/json",
-        dataType: 'json',
-        success:function( data )
-        {
-            var dataTosplit = data;
+            url: "/GradientArt",
+            type: 'POST',
+            data: JSON.stringify (pred),
+            contentType: "application/json",
+            dataType: 'json',
+            success:function( data ){
+                //this should be the image name
+                var res = dataTosplit.split(";");
+                var data = res[0];
+                var features = res[1];
+            }
+        })
+    return data;
+    }
+
+/*
+3b. print file names and affect vectors
+*/
+
+//Print data + features
+
+
+/*
+4a. Build Function to move gradient images across screen
+4b. set up "more like this" buttons to move between anchor images
+*/
+
+/*
+BELOW CODE IS FOR STEPS 4A AND 4B
+
+
+  var dataTosplit = data;
             var res = dataTosplit.split(";");
             var data = res[0];
             var features = res[1];
@@ -224,15 +254,7 @@ C0 = left
             img.id = "targetimg";
             $('#myInnerDiv').append(img);
 
-
-
-
-        }
-    })
-}
-
-
-
+*/
 
 
 
