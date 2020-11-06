a782138e9298b895eefc685a0c2a3b86760a849a
Sarah <smfrost@ucsc.edu>
2020-11-06 10:39:51 -0800

    Update sliders_functions.js

--- static/js/sliders_functions.js
+++ static/js/sliders_functions.js
@@ -67,7 +67,7 @@ return pred
 $(document).ready(function()
 {
 
-    $( '#angerValue,#anxietyValue,#socialValue,#positiveValue,#affiliationValue' ).mouseup(function() {
+    $( '#angerValue,#anxietyValue,#sadnessValue,#positiveValue,#affiliationValue' ).mouseup(function() {
         console.log('starting submit')
         pred = saveAffects()
     $.ajax({
